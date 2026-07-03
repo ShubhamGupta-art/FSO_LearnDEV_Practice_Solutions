8d41e6a16f3ef21a154061c6149dc1b68d273fa4
SHUBHAM GUPTA <sgprimaryid@gmail.com>
2026-07-04 02:06:01 +0530

    Update courseinfo App.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,17 @@
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
+import { useState } from "react"
 
-const Parts = (props) => {
-  return (
-    <p>
-      {props.part} ,{props.exercise}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Parts part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Parts part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Parts part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  );
-};
-const Total = ({parts}) => {
-  return <p> Number of exercises : {parts[0].exercises +parts[1].exercises+parts[2].exercises}</p>;
-};
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter ,setCounter] = useState(0)
+  
+  const handleClick=()=>{
+    console.log("clicked")
   }
+  return (<>
+    <div>{counter}</div>
+    <button onClick={()=> setCounter(counter+1)}> plus</button>
+  </>
+  )
+}
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
-export default App;
+export default App
